d8c1202d3402efa2acc77a6bb2cd2204112b5c7b
Gustavo Pals <gustavoandrepals@gmail.com>
2023-01-22 20:24:56 -0300

    feat: handleToggleHabit

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -23,7 +23,7 @@ interface Props extends TouchableOpacityProps {
 
 export function HabitDay({ amount = 0, completed = 0, date, ...rest }: Props) {
 	const amountAccomplishedPercentage =
-		amount > 0 ? generateProgressPercentage(completed, amount) : 0;
+		amount > 0 ? generateProgressPercentage(amount, completed) : 0;
 
 	const today = dayjs().startOf("day").toDate();
 	const isCurrentDay = dayjs(date).isSame(today);
@@ -31,21 +31,21 @@ export function HabitDay({ amount = 0, completed = 0, date, ...rest }: Props) {
 	return (
 		<TouchableOpacity
 			className={clsx("rounded-lg border-2 m-1", {
-				"bg-zinc-900 border-zinc-800":
+				["bg-zinc-900 border-zinc-800"]:
 					amountAccomplishedPercentage === 0,
-				"bg-violet-900 border-violet-700":
+				["bg-violet-900 border-violet-700"]:
 					amountAccomplishedPercentage > 0 &&
 					amountAccomplishedPercentage < 20,
-				"bg-violet-800 border-violet-600":
+				["bg-violet-800 border-violet-600"]:
 					amountAccomplishedPercentage >= 20 &&
 					amountAccomplishedPercentage < 40,
-				"bg-violet-700 border-violet-500":
+				["bg-violet-700 border-violet-500"]:
 					amountAccomplishedPercentage >= 40 &&
 					amountAccomplishedPercentage < 60,
-				"bg-violet-600 border-violet-400":
+				["bg-violet-600 border-violet-400"]:
 					amountAccomplishedPercentage >= 60 &&
 					amountAccomplishedPercentage < 80,
-				"bg-violet-500 border-violet-300":
+				["bg-violet-500 border-violet-300"]:
 					amountAccomplishedPercentage >= 80,
 				["border-white border-4"]: isCurrentDay,
 			})}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,8 +2,8 @@ import { Alert, ScrollView, Text, View } from "react-native";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
 import { generateRangeDatesFromYearStart } from "../utils/generate-range-between-dates";
-import { useNavigation } from "@react-navigation/native";
-import { useEffect, useState } from "react";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
+import { useCallback, useState } from "react";
 import { api } from "../lib/axios";
 import { Loading } from "../components/Loading";
 import dayjs from "dayjs";
@@ -25,9 +25,11 @@ export function Home() {
 	const [summary, setSummary] = useState<SummaryProps | null>(null);
 	const { navigate } = useNavigation();
 
-	useEffect(() => {
-		fetchData();
-	}, []);
+	useFocusEffect(
+		useCallback(() => {
+			fetchData();
+		}, [])
+	);
 
 	async function fetchData() {
 		try {
